d6ae6f18003ebaf71404e32ff16d5503c16d7543
Omayemi Agbeyegbe <grend500@gmail.com>
2017-08-02 15:34:49 -0400

    add template rendering and session storage

--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -24,6 +24,7 @@ class ControllerBase
     @already_built_response = true
     res['Location']=url
     res.status = 302
+    session.store_session(res)
   end
 
   # Populate the response with content.
@@ -34,6 +35,7 @@ class ControllerBase
     res['Content-Type'] = content_type
     res.write(content)
     @already_built_response = true
+    session.store_session(res)
   end
 
   # use ERB and binding to evaluate templates
@@ -47,6 +49,7 @@ class ControllerBase
 
   # method exposing a `Session` object
   def session
+    @session ||= Session.new(req)
   end
 
   # use this with the router to call action_name (:index, :show, :create...)
